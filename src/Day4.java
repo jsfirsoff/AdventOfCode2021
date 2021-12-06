2fbfe0016f04edde4ca5a31c438c26962cec1777
jsfirsoff <26422116+jsfirsoff@users.noreply.github.com>
2021-12-06 14:00:38 -0500

    Day4 change
    -deleted unused imports

--- src/Day4.java
+++ src/Day4.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Day4 {
